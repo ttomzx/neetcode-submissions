a70e8d31fe502a1a68688ad5f1559d0dbe85267e
Tolen Sorokhaibam <tolenkangleicha@gmail.com>
2026-08-19 15:40:05 +0530

    Add: number-of-senior-citizens - submission-5

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-5.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        c = 0
+        for detail in details:
+            age = int(detail[11:13])
+            if  age > 60:
+                c += 1
+
+        return c
